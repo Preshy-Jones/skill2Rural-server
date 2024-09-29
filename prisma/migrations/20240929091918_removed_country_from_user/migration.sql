f240480fa4cd237ae4303ce56af04b0ea5f1a376
Precious Adedibu <adedibuprecious@gmail.com>
2024-09-29 10:20:29 +0100

    add admin user model

---
+++ prisma/migrations/20240929091918_removed_country_from_user/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "AdminStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'SUSPENDED', 'DEACTIVATED', 'DELETED');
+
+-- AlterEnum
+ALTER TYPE "Type" ADD VALUE 'ADMIN';
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "status" "AdminStatus" NOT NULL DEFAULT 'INACTIVE',
+    "profile_photo" TEXT,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
