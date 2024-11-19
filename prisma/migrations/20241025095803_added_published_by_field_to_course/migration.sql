f67ece25dfca5b1bc41f0fb3631dd26957e287fc
Precious Adedibu <adedibuprecious@gmail.com>
2024-11-19 02:20:37 +0100

    temporarily removed migrations

--- prisma/migrations/20241025095803_added_published_by_field_to_course/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Course" ADD COLUMN     "publishedById" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Course" ADD CONSTRAINT "Course_publishedById_fkey" FOREIGN KEY ("publishedById") REFERENCES "Admin"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20241031013532_added_status_ofn_courses/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "CourseType" AS ENUM ('FREE', 'PAID');
-
--- AlterTable
-ALTER TABLE "Course" ADD COLUMN     "type" "CourseType" NOT NULL DEFAULT 'FREE';
